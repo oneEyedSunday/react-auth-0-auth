1775ae473c00d948cc4f8da2dff560a9da668d6b
Idiakose O. Sunday <idiakosesunday@gmail.com>
2019-11-21 05:02:03 +0100

    Auth methods

---
+++ src/Auth/Auth.js
@@ -0,0 +1,61 @@
+import auth0 from 'auth0-js';
+import { config } from './config';
+
+class Auth {
+    auth0 = new auth0.WebAuth({
+        domain: config.domain,
+        clientID: config.clientID,
+        redirectUri: config.redirectUri,
+        responseType: 'id_token',
+        scope: 'openid profile'
+    });
+
+    isAuthenticated() {
+        return new Date().getTime() < this.expiresAt;
+    }
+
+    signIn() {
+        this.auth0.authorize();
+    }
+
+    handleAuthentication() {
+        return new Promise((resolve, reject) => {
+            this.auth0.parseHash((err, authResult) => {
+                if (err) return reject(err);
+                if (!authResult || !authResult.idToken) {
+                    return reject(err);
+                }
+                this.setSession(authResult);
+                resolve();
+            });
+        })
+    }
+
+    setSession(authResult) {
+        this.idToken = authResult.idToken;
+        this.profile = authResult.idTokenPayload;
+        // set the time that the id token will expire at
+        this.expiresAt = authResult.idTokenPayload.exp * 1000;
+    }
+
+    signOut() {
+        this.auth0.logout({
+            returnTo: 'http://localhost:3000',
+            clientID: config.clientID,
+        });
+    }
+
+    silentAuth() {
+        return new Promise((resolve, reject) => {
+            this.auth0.checkSession({}, (err, authResult) => {
+                if (err) return reject(err);
+                this.setSession(authResult);
+                resolve();
+            });
+        });
+    }
+}
+
+const Auth0 = new Auth();
+
+export default Auth0;
